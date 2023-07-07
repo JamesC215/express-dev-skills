e774ee45fadf5ad59b2023b7e25b09f2456ef352
James Carter <136309778+JamesC215@users.noreply.github.com>
2023-07-07 23:53:41 +0100

    Update and edit skills 	modified:   controllers/skills.js 	modified:   models/skill.js 	modified:   public/stylesheets/style.css 	modified:   routes/skills.js 	new file:   views/skills/edit.ejs 	modified:   views/skills/index.ejs 	new file:   views/skills/new.ejs 	modified:   views/skills/show.ejs

--- controllers/skills.js
+++ controllers/skills.js
@@ -4,7 +4,41 @@ const Skill = require('../models/skill')
 module.exports = {
     index,
     show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update,
 };
+
+function update(req, res) {
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
+}
+ 
+function edit (req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/edit', {
+    title: 'Edit a skill',
+    skill
+  });
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
+
+function create(req, res) {
+  // Models are responsible for CRUD'ing the data
+  Skill.create(req.body);
+  // Always do a redirect when data has been changed
+  res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', { title: 'New Skill'});
+}
 
 function show(req, res) {
     res.render('skills/show', {
--- models/skill.js
+++ models/skill.js
@@ -1,7 +1,7 @@
 const skills = [
-    {id: 125223, skill: 'Learn HTML', learnt: true},
-    {id: 127904, skill: 'Learn CSS', learnt: true},
-    {id: 139608, skill: 'Learn JavaScript', learnt: true},
+    {id: 125223, skill: 'HTML', learnt: true},
+    {id: 127904, skill: 'CSS', learnt: true},
+    {id: 139608, skill: 'JavaScript', learnt: true},
     {id: 119848, skill: 'Express', learnt: false},
     {id: 184738, skill: 'MongoDB', learnt: false},
     {id: 124658, skill: 'Python', learnt: false},
@@ -10,8 +10,32 @@ const skills = [
   module.exports = {
     getAll,
     getOne,
+    create,
+    deleteOne,
+    update,
   };
 	
+  function update(id, updatedSkill) {
+    id = parseInt(id);
+    const skill = skills.find(skills => skills.id === id);
+    skill.skill = updatedSkill.skill;
+  }
+
+  function deleteOne(id) {
+    id = parseInt(id);
+    //find the index for the skill
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1)
+}
+
+  function create(skill) {
+    // Add the id
+    skill.id = Date.now() % 1000000;
+    // New todos wouldn't be done :)
+    skill.done = false;
+    skills.push(skill);
+  }
+
   function getOne(id) {
     id = parseInt(id);
     return skills.find(skills => skills.id === id);
